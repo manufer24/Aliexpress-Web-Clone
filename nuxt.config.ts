1ef4e470586c4e23b07c89fe72ffb948aa2123b4
manufer24 <manuelferg13@gmail.com>
2023-10-15 15:20:32 -0400

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,26 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  pages: true,
+  modules: [
+      'nuxt-icon',
+      'nuxt-lodash',
+      '@pinia/nuxt',
+      '@nuxtjs/tailwindcss',
+      '@nuxtjs/supabase'
+  ],
+  supabase: {
+    redirect: false
+  },
+  runtimeConfig: {
+      public: {
+        STRIPE_KEY: process.env.STRIPE_PK_KEY
+      }
+  },
+  app: {
+      head: {
+        script: [
+          { src: 'https://js.stripe.com/v3/', defer: true }
+        ],
+      }
+    }
+})
