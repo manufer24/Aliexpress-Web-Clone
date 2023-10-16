44bf45787cc639980edf2c3d584331fccf5b5249
manufer24 <manuelferg13@gmail.com>
2023-10-16 14:06:23 -0400

    fixing bug

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
       'nuxt-lodash',
       '@pinia/nuxt',
       '@nuxtjs/tailwindcss',
-      '@nuxtjs/supabase'
+      '@nuxtjs/supabase',
   ],
   supabase: {
     redirect: false
